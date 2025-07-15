ddbb2fd13aa254285793f010a5e11e4c31f05e5f
“Aleemuddin <aleem2747@gmail.com>
2025-07-15 05:31:01 -0500

    updating values}

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "4.0.2"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
 
-  name = "arcgis-vpc"
+  name = "arcgis-pe2-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs             = ["us-east-1a"]
+  public_subnets  = ["10.0.1.0/24"]
 
   enable_nat_gateway = false
   single_nat_gateway = false
@@ -21,7 +21,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.8.4"
 
-  cluster_name    = "arcgis-eks"
+  cluster_name    = "arcgis-pe2-eks"
   cluster_version = "1.29"
   subnet_ids      = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
